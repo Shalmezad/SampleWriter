a03496e1e32c83dac97ff939eb67bd532bf2b84a
Richard Wardin <shalmezad@gmail.com>
2013-05-08 20:55:41 -0400

    Added menu and frame array.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+int displayMenu();
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    //set up variables
+    int numFrames;
+    //prompt for numFrames
+    cout<<"Please enter the number of frames in this sample: "<<endl;
+    cin>>numFrames;
+
+    //setup the frames.
+    char* frames = new char[numFrames];
+    //get the sample type.
+    int sampleType = displayMenu();
+    //don't forget to delete the frames.
+    delete[] frames;
     return 0;
+}
+
+int displayMenu()
+{
+    int choice = 0;
+    do
+    {
+        //display the menu.
+        cout<<"Choose a wave type:"<<endl;
+        cout<<"1) Square"<<endl;
+        cout<<"2) Sawtooth (/|)"<<endl;
+        cout<<"3) Sin"<<endl;
+        cout<<"4) Triangle"<<endl;
+        cin>>choice;
+    }while(choice<=0 || choice>4);
+    return choice;
 }
