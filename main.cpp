509c4b79fffa18e513ffe2ffe55c788c49388288
Richard Wardin <shalmezad@gmail.com>
2013-05-08 21:18:26 -0400

    Added sine wave generator

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -7,6 +8,7 @@ using namespace std;
 int displayMenu();
 void squareWave(char*, int);
 void sawWave(char*, int);
+void sinWave(char*, int);
 void triangleWave(char*, int);
 
 int main()
@@ -30,6 +32,9 @@ int main()
             break;
         case 2:
             sawWave(frames, numFrames);
+            break;
+        case 3:
+            sinWave(frames, numFrames);
             break;
         case 4:
             triangleWave(frames, numFrames);
@@ -62,6 +67,22 @@ void sawWave(char* frames, int numFrames){
     for(int a=0; a<numFrames;a++){
         frames[a] = ((float)a/numFrames) * (128) + 128;
     }
+}
+
+void sinWave(char* frames, int numFrames){
+    //this was the one wave I was dreading...
+    //Alright, think about this logically.
+    //Sin is -1 to 1, I need 0 to 255,
+    //so, sin(a) * 128 + 128
+    //I also need to look at the x values.
+    //a "cycle" of sine is 0 to 2pi.
+    //I need to change this cycle from 0 to numFrames.
+    //so, sin(x * 2pi / numFrames)
+    //combined with what I have above.
+    for(int a=0; a<numFrames; a++){
+        frames[a] = sin((float)a * 2 * 3.14 / numFrames) * 128 + 128;
+    }
+    //here's hoping I got that right...
 }
 
 void triangleWave(char* frames, int numFrames){
